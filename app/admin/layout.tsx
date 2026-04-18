10c2fa7efa18d4961b5717ec3cae7b25e6af9667
Bruno <bruno@example.com>
2026-04-18 02:16:59 -0300

    fix: enforce dynamic ssr on admin layout to prevent SSG prisma build timeouts

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import AdminSidebar from "@/components/admin/AdminSidebar";
 
+export const dynamic = 'force-dynamic';
+
 export default function AdminLayout({
   children,
 }: {
